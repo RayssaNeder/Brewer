144d3a3785f22b35dbe8373649eae20e94948d07
Rayssa Freitas <rayssafreitas13@gmail.com>
2019-03-31 18:25:33 -0300

    Utilizando validação do org.hibernate.validator; Utilizando forward e redirect.

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,8 +1,13 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -15,9 +20,17 @@ public class CervejasController {
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(Cerveja cerveja) {
-		System.out.println(">>> sku: " + cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
+		}
+		
+		// Salvar no banco de dados...
+			attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+			System.out.println(">>> sku: " + cerveja.getSku());
+			return "redirect:/cervejas/novo";
 	}
 	
 }
--- brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,7 +1,10 @@
 package com.algaworks.brewer.model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 public class Cerveja {
 
+	@NotBlank
 	private String sku;
 	private String nome;
 	
