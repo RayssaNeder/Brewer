fc8334e27285b5fb173b2b495a9976cd14a456ff
Rayssa Freitas <rayssafreitas13@gmail.com>
2019-04-02 19:26:28 -0300

    Configurando o Spring Data

--- brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -4,14 +4,14 @@ import javax.servlet.Filter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-
+import com.algaworks.brewer.config.JPAConfig;
 import com.algaworks.brewer.config.WebConfig;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class<?>[] { JPAConfig.class };
 	}
 
 	@Override
--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.repository.Cervejas;
 
 @Controller
 public class CervejasController {
+	
+	@Autowired
+	private Cervejas cervejas;
 
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
+		
+		cervejas.findAll();
 		return "cerveja/CadastroCerveja";
 	}
 	
