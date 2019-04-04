b5ad1fed163ba016406509ce6eaaab816c632c50
Rayssa Freitas <rayssafreitas13@gmail.com>
2019-04-03 21:25:08 -0300

    Implementando combos de estilo e Sabor e radio de Origem

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -8,26 +8,35 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
 import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
 	
 	@Autowired
 	private Cervejas cervejas;
+	@Autowired
+	private Estilos estilos;
 
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
 		
-		cervejas.findAll();
-		return "cerveja/CadastroCerveja";
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
 		if (result.hasErrors()) {
 			return novo(cerveja);
@@ -36,7 +45,7 @@ public class CervejasController {
 		// Salvar no banco de dados...
 			attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 			System.out.println(">>> sku: " + cerveja.getSku());
-			return "redirect:/cervejas/novo";
+			return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 }
