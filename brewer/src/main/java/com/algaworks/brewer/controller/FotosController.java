2657b06b3b7867493af403ee9703768db5732973
Rayssa Freitas <rayssafreitas13@gmail.com>
2019-06-03 19:07:34 -0300

    Melhorando a disponibilidade da aplicação - retorno assíncrono

--- brewer/src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -4,15 +4,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.brewer.storage.FotoStorageRunnable;
 
 @RestController
 @RequestMapping("/fotos")
 public class FotosController {
 	
 	@PostMapping
-	public String upload(@RequestParam("files[]") MultipartFile[] files) {
-		System.out.println("File >>>> " + files[0].getName() + " " + files[0].getSize());
-		return "ok";
+	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files) {		
+		DeferredResult<String> resultado = new DeferredResult<>();
+		
+		
+		Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
+		thread.start();
+		
+		
+		return resultado;
 	}
 }
---
+++ brewer/src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -0,0 +1,23 @@
+package com.algaworks.brewer.storage;
+
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FotoStorageRunnable implements Runnable {
+	
+	private MultipartFile[] files;
+	private DeferredResult<String> resultado;
+
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<String> resultado) {
+		this.files = files;
+		this.resultado = resultado;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("File >>>> " + files[0].getName() + " " + files[0].getSize());
+		//TODO: Salvar foto no sistema de arquivos
+		resultado.setResult("OK! foto recebida!");
+	}
+
+}
