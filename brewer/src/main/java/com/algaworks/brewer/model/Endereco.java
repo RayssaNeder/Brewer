dc9ba028f523d70b4d480b2e09582949f688395e
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-01-19 15:22:03 -0200

    Finalizando pesquisa de clientes

--- brewer/src/main/java/com/algaworks/brewer/model/Endereco.java
+++ brewer/src/main/java/com/algaworks/brewer/model/Endereco.java
@@ -67,6 +67,13 @@ public class Endereco implements Serializable {
 		this.estado = estado;
 	}
 	
+	public String getNomeCidadeSiglaEstado() {
+		if(this.cidade != null) {
+			return this.cidade.getNome() + "/" + this.cidade.getEstado().getSigla();
+		}
+		return null;
+	}
+	
 	
 
 	
--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/ClientesImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/ClientesImpl.java
@@ -9,6 +9,7 @@ import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Projection;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -36,6 +37,8 @@ public class ClientesImpl implements ClientesQueries {
 		
 		paginacaoUtil.preparar(criteria, pageable);
 		adicionarFiltro(clienteFilter, criteria);
+		criteria.createAlias("endereco.cidade", "c", JoinType.LEFT_OUTER_JOIN);
+		criteria.createCriteria("c.estado", "e", JoinType.LEFT_OUTER_JOIN);
 		return new PageImpl<>(criteria.list(), pageable, total(clienteFilter));
 	}
 
