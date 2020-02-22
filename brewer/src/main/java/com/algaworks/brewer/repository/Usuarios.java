51c1afebdda0be9171486927f0eda480b4db3ead
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-02-22 12:46:12 -0300

    Carregando as permissões do usuário

--- brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,6 @@ import com.algaworks.brewer.repository.helper.usuario.UsuariosQueries;
 public interface Usuarios extends JpaRepository <Usuario, Long>, UsuariosQueries {
 
 	public Optional<Usuario> findByEmailIgnoreCase(String email);
+
 
 }
--- brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -1,9 +1,13 @@
 package com.algaworks.brewer.repository.helper.usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.algaworks.brewer.model.Usuario;
 
 public interface UsuariosQueries {
+	
 	public Optional<Usuario> porEmailEAtivo(String email);
+	
+	public List<String> permissoes(Usuario usuario);
 }
