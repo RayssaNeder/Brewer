3390dc9d5e28173a7c51e9a9c4fbd13a04d47b12
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-02-23 10:53:05 -0300

    Adicionando tela de acesso negado

--- brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -47,6 +47,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginPage("/login")
 			.permitAll()
 			.and()
+		.exceptionHandling()
+			.accessDeniedPage("/403")
+			.and()
 		.csrf().disable();
 	}
 	
