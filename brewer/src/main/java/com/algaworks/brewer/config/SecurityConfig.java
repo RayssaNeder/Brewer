1c27604e75b624d7b0c413c170ef9e864662824d
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-02-23 23:30:12 -0300

    Expirando sessão com novo login dpara o mesmo usuario

--- brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -52,7 +52,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 			.and()
 		.exceptionHandling()
-			.accessDeniedPage("/403");
+			.accessDeniedPage("/403")
+			.and()
+		.sessionManagement()
+			.maximumSessions(1)
+			.expiredUrl("/login");
 	}
 	
 	@Bean
