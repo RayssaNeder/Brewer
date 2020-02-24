a1139467206bf9ebfc532fb816047570e93ed35f
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-02-23 23:15:13 -0300

    Problema de acentuação voltou devido à implementação da segurança CSRF

--- brewer/src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
+++ brewer/src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -1,7 +1,17 @@
 package com.algaworks.brewer.config.init;
 
+import javax.servlet.FilterRegistration;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
-
+	
+	protected void beforeSpringSecurityFilterChain(javax.servlet.ServletContext servletContext) {
+		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter",
+				new CharacterEncodingFilter());
+		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+		characterEncodingFilter.setInitParameter("forceEncoding", "true");
+		characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
+	};
 }
