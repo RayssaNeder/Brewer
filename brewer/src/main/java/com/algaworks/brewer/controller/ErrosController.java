c8813bd30ee1178dd61537d157193617ebda3137
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-02-25 13:28:50 -0300

    Desafio: criando a página 500 - Erro no servidor

--- brewer/src/main/java/com/algaworks/brewer/controller/ErrosController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/ErrosController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrosController {
@@ -10,4 +11,11 @@ public class ErrosController {
 	public String paginaNaoEcontrada() {
 		return "404";
 	}
+	
+	
+	@RequestMapping("/500")
+	public String erroInternoServidor() {
+		return "500";
+	}
+	
 }
