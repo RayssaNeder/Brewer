13adfd72f3239996352c58d98cbccf65f3548572
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-02-25 17:12:44 -0300

    Implementando o menu lateral

--- brewer/src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
+++ brewer/src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import com.algaworks.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import com.algaworks.brewer.thymeleaf.processor.MenuAttributeTagProcessor;
 import com.algaworks.brewer.thymeleaf.processor.MessageElementTagProcessor;
 import com.algaworks.brewer.thymeleaf.processor.OrderElementTagProcessor;
 import com.algaworks.brewer.thymeleaf.processor.PaginationElementTagProcessor;
@@ -25,6 +26,7 @@ public class BrewerDialect extends AbstractProcessorDialect{
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		processadores.add(new PaginationElementTagProcessor(dialectPrefix));
+		processadores.add(new MenuAttributeTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
