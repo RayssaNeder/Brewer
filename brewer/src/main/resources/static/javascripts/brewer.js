345e89d4293a1236feb589ca64658f06b03fb149
Rayssa Freitas <rayssafreitas13@gmail.com>
2020-03-15 17:22:31 -0300

    Renderizando cervejas no autoComplete

--- brewer/src/main/resources/static/javascripts/brewer.js
+++ brewer/src/main/resources/static/javascripts/brewer.js
@@ -84,6 +84,13 @@ Brewer.Security = (function(){
 }());
 
 
+Brewer.formatarMoeda = function(valor){
+	numeral.locale('pt-br');
+	return numeral(valor).format('0,0.00');
+	
+}
+
+
 
 
 $(function() {
